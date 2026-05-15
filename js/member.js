cbb6b22c207a042b43ba1588d5667507c890ad43
Ivy Rao <ivyrao@microsoft.com>
2026-05-15 22:50:20 +0800

    init: gala2026 lottery project

---
+++ js/member.js
@@ -0,0 +1,19 @@
+var member = [];
+
+for (var i = 0; i < 100; i++) {
+  if (i < 9) {
+    member[i] = {
+      name: "00" + (i + 1) + "号",
+    };
+  }
+  else if (i < 99) {
+    member[i] = {
+      name: "0" + (i + 1) + "号",
+    };
+  }
+  else {
+    member[i] = {
+        name: (i + 1) + "号",
+    };
+  }
+}
